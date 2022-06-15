6ac2b610c827512f889b4e50015c961c213f272c
Fauzi Maulana Habibi <fauzimaulanahabibi@gmail.com>
2022-06-15 23:48:49 +0700

    create homepage

---
+++ theme/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+	<strong>Copyright &copy; 2022 <a href="">Kelompok 1 Unindra S6I</a>.</strong>
+	All rights reserved.
+	<div class="float-right d-none d-sm-inline-block">
+		<b>AdminLTE Version</b> 3.2.0
+	</div>
+</footer>
---
+++ theme/sidebar.php
@@ -0,0 +1,137 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+	<!-- Brand Logo -->
+	<a href="" class="brand-link">
+		<img src="dist/img/tutwurihandayani-logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<span class="brand-text font-weight-light">SMKN 19 Jakarta</span>
+	</a>
+
+	<!-- Sidebar -->
+	<div class="sidebar">
+		<!-- Sidebar user panel (optional) -->
+		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+			<div class="image">
+				<img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+			</div>
+			<div class="info">
+				<a href="#" class="d-block">Alexander Pierce</a>
+			</div>
+		</div>
+
+		<!-- Sidebar Menu -->
+		<nav class="mt-2">
+			<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+				<!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+				<li class="nav-item">
+					<a href="./index.php" class="nav-link active">
+						<i class="nav-icon fas fa-tachometer-alt"></i>
+						<p>Dashboard</p>
+					</a>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-book"></i>
+						<p>Data Master<i class="fas fa-angle-left right"></i></p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="pages/forms/general.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>General Elements</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/forms/advanced.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Advanced Elements</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/forms/editors.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Editors</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/forms/validation.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Validation</p>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-edit"></i>
+						<p>Data Transaction<i class="fas fa-angle-left right"></i></p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="pages/examples/invoice.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Invoice</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/examples/profile.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Profile</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/examples/e-commerce.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>E-commerce</p>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<li class="nav-item">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-chart-pie"></i>
+						<p>
+							Report
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="pages/charts/chartjs.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>ChartJS</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/charts/flot.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Flot</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/charts/inline.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Inline</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="pages/charts/uplot.html" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>uPlot</p>
+							</a>
+						</li>
+					</ul>
+				</li>
+
+				<br><br>
+
+				<li class="nav-item">
+					<a href="./index.php" class="nav-link bg-danger">
+						<i class="nav-icon fas fa-sign-out-alt"></i>
+						<p>Log Out</p>
+					</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- /.sidebar-menu -->
+	</div>
+	<!-- /.sidebar -->
+</aside>
